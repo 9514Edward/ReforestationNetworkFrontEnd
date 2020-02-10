a8d8b98e9e39e6b84adb8334295843f6816e8065
efelsenthal <58897730+efelsenthal@users.noreply.github.com>
2020-02-09 21:16:12 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "reforestation-network"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://qex26njog3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_VqPeBXjex",
+    APP_CLIENT_ID: "1ckmn8gfekrq6cipoif3tamdi5",
+    IDENTITY_POOL_ID: "us-east-1:a4a8b376-39b1-4def-918e-f63dfbff6eb1"
+  }
+};
